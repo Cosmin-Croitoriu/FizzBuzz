36bdc44017f7a24e73d7f2a18301b3490ad4615f
Cosmin <cosmin.c.croitoriu@gmail.com>
2019-04-27 13:51:27 +0100

    method returns buzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,8 @@ describe "fizzbuzz" do
   it "returns fizz when it takes 3 as an argumet" do
     expect(fizzbuzz(3)).to eq("fizz")
   end
+
+  it "returns buzz when it takes 5 as an argument" do
+    expect(fizzbuzz(5)).to eq("buzz")
+  end
 end
