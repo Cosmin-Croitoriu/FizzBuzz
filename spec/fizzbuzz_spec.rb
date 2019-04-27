0fdb09cd4393cb9f0a4fd9c36a893775c10e03d6
Cosmin <cosmin.c.croitoriu@gmail.com>
2019-04-27 13:46:03 +0100

    method returns fizz when argument multiple of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,11 @@
 require "./fizzbuzz.rb"
 
 describe "fizzbuzz" do
-  it "takes 1 as an argument and returns 1" do
+  it "returns 1 when it takes 1 as an argument" do
     expect(fizzbuzz(1)).to eq(1)
+  end
+
+  it "returns fizz when it takes 3 as an argumet" do
+    expect(fizzbuzz(3)).to eq("fizz")
   end
 end
